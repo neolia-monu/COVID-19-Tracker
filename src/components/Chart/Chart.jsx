d0c2a2191fcb07bac4396e75c40cab24a0e8719f
Monu Neolia <92224200+neolia-monu@users.noreply.github.com>
2022-10-15 15:50:49 +0530

    initial commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,31 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
 import { Line, Bar } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
 
 import styles from "./Chart.module.css";
 
-export const Chart = () => {
-  const [dailyData, setDailyData] = useState({});
+Chart.register(...registerables);
+
+export const CovidChart = () => {
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
-      const dailyData = await fetchDailyData();
+      const dailyData1 = await fetchDailyData();
 
-      setDailyData(dailyData);
-
-      console.log(dailyData);
-
-      fetchAPI();
+      setDailyData(dailyData1);
     };
-  }, []);
 
-  const lineChart = dailyData[0] ? (
-    <Line data={{ labels: "", datasets: [{}, {}] }} />
+    console.log(dailyData);
+
+    fetchAPI();
+  });
+
+  // const lineChart = dailyData ? <h1>Charts</h1> : null;
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Recovered",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true
+          }
+        ]
+      }}
+    />
   ) : null;
-
-  return (
-    <>
-      <h1>Chart</h1>
-    </>
-  );
+  // // (
+  //   <Line data={{ labels: "", datasets: [{}, {}] }} />
+  //   )
+  return <div className={styles.container}>{lineChart}</div>;
 };
